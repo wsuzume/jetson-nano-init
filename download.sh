dffb34fe0d4db617ddf665435c7e4a511cff54ec
wsuzume <wspyy9584@gmail.com>
2019-11-10 20:59:09 +0900

    add git clone HRNet

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# download HRNet and unzip
+# download HRNet
+git clone https://github.com/leoxiaobin/deep-high-resolution-net.pytorch
+
+# download pretrained HRNet params and unzip
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1deN4MYiNyEEsv8WeVXpX7DsI5FkPmOdB" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1deN4MYiNyEEsv8WeVXpX7DsI5FkPmOdB" -o hrnet.zip
